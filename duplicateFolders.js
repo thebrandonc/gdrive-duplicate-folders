e445ed334f37d0461665559bacd1fbfc34dc8379
Brandon C <b@gridabl.com>
2022-08-19 13:57:18 -0700

    add method for getting folder id from link

--- duplicateFolders.js
+++ duplicateFolders.js
@@ -10,10 +10,17 @@ const foldersToCopy = 'Sheet1';  // tab name containing list of folders to dupli
 
 
 class TemplateDir {
-  getTemplate(templateDirId) {
+    getIdFromLink(folderLink) {
+    let id;
+    id = folderLink.split(/[?\/]+/);
+    this.id = id[id.length-2];
+  };
+
+  getTemplate() {
     try {
-      this.folder = DriveApp.getFolderById(templateDirId);
+      this.folder = DriveApp.getFolderById(this.id);
       this.parentDir = this.getParentFolder(this.folder);
+
     } catch(err) {
       const msg = {
           type: '😞 Something went wrong',
@@ -26,16 +33,19 @@ class TemplateDir {
   getParentFolder(folder) {
     try {  
       const parents = folder.getParents();
+
       while (parents.hasNext()) {
         if (!this.parent) {
           return parents.next();
         };
       };
+
     } catch(err) {
       const msg = {
           type: '😞 Something went wrong',
           msg: `There was a problem finding the parent folder. ${err}`
         };
+
         new Notification().send(msg);
     };
   };
@@ -63,6 +73,7 @@ class CopyManager {
     try {
       this.templateDir = templateDir;
       this.destinationDir = destinationDir;
+
       while (this.foldersInspected < this.numFolders) {
         const parentFolder = this.foldersInspected === 0 ? true : false;
         const source = parentFolder ? this.templateDir : this.queue[0].source;
@@ -72,20 +83,24 @@ class CopyManager {
         if (!parentFolder) this.queue.shift();
         this.foldersInspected += 1;
       };
+
       if (this.numFolders === this.foldersInspected) {
         this.processComplete = true;
       };
+
     } catch(err) {
         const msg = {
           type: '😞 Something went wrong',
           msg: `There was a problem copying a folder. ${err}`
         };
+
         new Notification().send(msg);
     };
   };
 
   copyFolders(source, destination) {
     const folders = source.getFolders();
+
     while (folders.hasNext()) {
       const subDir = folders.next();
       const newSubDir = destination.createFolder(subDir.getName());
@@ -96,6 +111,7 @@ class CopyManager {
 
   copyFiles(source, destination) {
     const files = source.getFiles();
+
     while (files.hasNext()) {
       const file = files.next();
       file.makeCopy(file.getName(), destination);
@@ -119,7 +135,9 @@ class Sheet {
         folder.push(i+1);
         if (folder[0] === '') this.foldersToDupe.push(folder);
       });
+      
       return this.foldersToDupe;
+
     } catch(err) {
         const msg = {
           type: '😞 Something went wrong',
@@ -148,36 +166,44 @@ class App {
   run() {
     try {
       this.foldersToDupe.forEach(folder => {
-        const [_, templateDirId, newDirName] = folder;
+        const [_, templateLink, newDirName] = folder;
         const templateDir = new TemplateDir();
-        const destinationDir = new DestinationDir()
-        const copy = new CopyManager()
-        templateDir.getTemplate(templateDirId);
+        const destinationDir = new DestinationDir();
+        const copy = new CopyManager();
+
+        templateDir.getIdFromLink(templateLink);
+        templateDir.getTemplate();
         destinationDir.create(templateDir.parentDir, newDirName);
         copy.create(templateDir.folder, destinationDir.folder);
+
         if (copy.processComplete) {
           this.sheet.markComplete(folder);
           this.completedCopies += 1;
-        }
+        };
       });
       let msg;
+
       if (this.completedCopies > 0) {
         msg = { 
           type: '🏆 Success', 
           msg: `${this.completedCopies} folder(s) successfully copied!` 
         };
+
       } else {
         msg = { 
           type: 'Copying Complete', 
           msg: `No folders were copied.` 
         };
       };
+
       new Notification().send(msg);
+
     } catch(err) {
       const msg = {
         type: '😞 Something went wrong ',
         msg: err
       };
+      
       new Notification().send(msg);
     };
   };
